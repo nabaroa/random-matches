c608ba0bab9b99a8c58c9acf02ff967f1fc3ee1e
NAIARA ABAROA <naiaraabaroa@gmail.com>
2016-12-10 18:30:40 +0100

    First release

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+// nakPostCSS gulpfile by @nabaroa
+var gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssimport = require('postcss-import'),
+    customproperties = require('postcss-custom-properties'),
+    apply = require('postcss-apply'),
+    mixins = require('postcss-mixins'),
+    nested = require('postcss-nested'),
+    customMedia = require("postcss-custom-media")
+    nano = require('gulp-cssnano'),
+    notify = require('gulp-notify');
+
+gulp.task('css', function() {
+    var processors = [
+      cssimport,
+      autoprefixer,
+      customproperties,
+      apply,
+      mixins,
+      nested,
+      customMedia
+    ];
+    var configNano = {
+      autoprefixer: { browsers: 'last 2 versions' },
+      discardComments: { removeAll: true },
+      safe: true
+    };
+    return gulp.src('./nakDS-src/*.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./dist'))
+        .pipe(nano(configNano))
+        .pipe(gulp.dest('./docs/css'))
+        .pipe(notify({ message: 'Your CSS is ready ♡' }));
+});
+// Watch
+gulp.task('watch', function() {
+    // Watch .css files
+    gulp.watch('nakDS-src/**/*.css', ['css']);
+
+});
+
+// Default
+gulp.task('default', ['css', 'watch',]);
